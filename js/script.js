f7044e0427f76af5d93de842665e00021cf6592e
subaruice <tohaermakov11111@gmail.com>
2022-10-21 20:33:33 +0300

    cycle lesson

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const userData = +prompt("Сколько вам лет?", "");
 
 console.log(userData);
-const category = 'films';
+const category = "films";
 console.log(`https://google.com/${category}25`);
 
 /* Задание на урок:
@@ -25,16 +25,51 @@ console.log(`https://google.com/${category}25`);
     }
 Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-const  personalMovieDB = {
-    count : numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
 };
-const userLastWatched = prompt('Один из последних просмотренных фильмов?', '');
-const userRate = prompt('На сколько оцените его?', '');
+const userLastWatched = prompt("Один из последних просмотренных фильмов?", "");
+const userRate = prompt("На сколько оцените его?", "");
 personalMovieDB.movies[userLastWatched] = userRate;
 
 console.log(personalMovieDB.movies);
+
+//-------------------------------------cycle-----------------------------------
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = "";
+
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+	result += "\n";
+	for (let j = 0; j < i; j++) {
+		result += "*";
+	}
+}
+
+
+console.log(result);
+
+
+
+first: for (let i = 1; i < length; i++) {
+	console.log(`Third lvl: ${i}`);
+	for (let j = 0; j < i; j++) {
+		console.log(`Third lvl: ${i}`);
+		for (let k = 0; k < i; k++) {
+			if (k === 2) continue first;
+			console.log(`Third lvl: ${k}`);
+		}
+	}
+}
